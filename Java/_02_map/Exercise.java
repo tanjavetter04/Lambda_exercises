cdd10d6c11eca2bc3a14f00b71fdb31e9f4f2758
tanjavetter04 <126447698+tanjavetter04@users.noreply.github.com>
2024-05-26 14:55:21 +0200

    Update Exercise.java

--- Java/_02_map/Exercise.java
+++ Java/_02_map/Exercise.java
@@ -9,10 +9,5 @@ public class Exercise {
     public static void main(String[] args) {
         List<Integer> temperatures_celsius = List.of(25, 30, 22, 18, 28, 35, 20);
 
-        List<Double> temperature_fahrenheit = temperatures_celsius.stream()
-                                                .map(c -> (c * 9.0 / 5) + 32)
-                                                .collect(Collectors.toList());
-
-        System.out.println(temperature_fahrenheit);
     }
 }
