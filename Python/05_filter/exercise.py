51088b1edec85411a8074286b82cbdd575179d8d
tanjavetter04 <126447698+tanjavetter04@users.noreply.github.com>
2024-05-26 13:40:08 +0200

    Update exercise.py

--- Python/05_filter/exercise.py
+++ Python/05_filter/exercise.py
@@ -1,6 +1,3 @@
 # Gib mithilfe einer Lambda Funktion in Europa gemessene Temperaturen aus
 
 temperatures_continents = [('Asia', 32),('Asia', 28),('Europe', 20),('North America', 25),('Europe', 30),('Antarctica', -40),('North America', 35),('Europe', 18),('North America', 22),('Europe', 28)]
-
-europe_temperatures = filter(lambda x: x[0] == 'Europe', temperatures_continents)
-print(list(europe_temperatures))
