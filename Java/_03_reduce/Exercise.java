fbc6cf055eb7e4b1ae5badf42381c915fa1717bc
tanjavetter04 <126447698+tanjavetter04@users.noreply.github.com>
2024-05-26 14:55:37 +0200

    Update Exercise.java

--- Java/_03_reduce/Exercise.java
+++ Java/_03_reduce/Exercise.java
@@ -8,9 +8,5 @@ public class Exercise {
     public static void main(String[] args) {
         List<Integer> temperatures_celsius = List.of(25, 30, 22, 18, 28, 35, 20);
 
-        double average_temperature = temperatures_celsius.stream()
-                    .reduce(0, (x, y) -> x + y) / (double)temperatures_celsius.size();
-
-        System.out.println(average_temperature);
     }
 }
