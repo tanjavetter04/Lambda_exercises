5c7b0139a6ad88b9bdf93cae7c140ec739548573
tanjavetter04 <126447698+tanjavetter04@users.noreply.github.com>
2024-05-26 13:40:21 +0200

    Update cities_max.py

--- Python/06_extras/cities_max.py
+++ Python/06_extras/cities_max.py
@@ -1,6 +1,3 @@
 # Finde alle Städte in denen mindestens eine Temperatur über 30 Grad liegt mithilfe einer Lambda Funktion
 
 cities_max_temperatures = [('New York', [15, 25, 30]), ('Los Angeles', [20, 28, 32]), ('Chicago', [10, 20, 25]), ('Houston', [25, 30, 35])]
-
-hot_cities = filter(lambda city: any(temp > 30 for temp in city[1]), cities_max_temperatures)
-print(list(hot_cities))
