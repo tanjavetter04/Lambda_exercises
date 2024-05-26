6516a8e91a9194409cb7f92983fe24d93dbe6002
tanjavetter04 <126447698+tanjavetter04@users.noreply.github.com>
2024-05-26 13:39:42 +0200

    Update exercise.py

--- Python/03_reduce/exercise.py
+++ Python/03_reduce/exercise.py
@@ -1,8 +1,3 @@
 # Berechne die Durchschnittstemperatur mithilfe einer Lambda Funktion
 
-from functools import reduce
-
 temperatures_celsius = [25, 30, 22, 18, 28, 35, 20]
-
-average_temperature = reduce(lambda x, y: x + y, temperatures_celsius) / len(temperatures_celsius)
-print(average_temperature)
