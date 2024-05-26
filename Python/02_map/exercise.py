cefac4d10ba4e44abc8aba56ef53310d59d42b99
tanjavetter04 <126447698+tanjavetter04@users.noreply.github.com>
2024-05-26 13:39:26 +0200

    Update exercise.py

--- Python/02_map/exercise.py
+++ Python/02_map/exercise.py
@@ -1,6 +1,3 @@
 # Rechne die gegebenen Temperaturen mithilfe einer Lambda Funktion in Fahrenheit um (Temperatur * 1.8 + 32)
 
 temperatures_celsius = [25, 30, 22, 18, 28, 35, 20]
-
-temperatures_fahrenheit = map(lambda c: (c * 9/5) + 32, temperatures_celsius)
-print(list(temperatures_fahrenheit))
